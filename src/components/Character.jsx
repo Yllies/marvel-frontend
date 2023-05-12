1f0e8c10984bb30bfa47ac18fe32e4f2f559cfe6
Yllies HADJ-ABED <Yllies@MacBook-Air-Yllies.local>
2023-05-12 18:24:01 +0200

    update

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,13 +1,20 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 const Character = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState({});
+  const { name } = useParams();
+
+  console.log(name);
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get("http://localhost:3000/characters");
+        const response = await axios.get(
+          `http://localhost:3000/characters?name=${name}`
+        );
         console.log(response.data);
+        setData(response.data);
         setIsLoading(false);
       } catch (error) {
         console.log(error.response);
